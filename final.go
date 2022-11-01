d05127187eb4060c1b9c3111cd967ccc901a2b0b
SabbirAhmed <sabbir15-12867@diu.edu.bd>
2022-11-02 00:19:08 +0600

    Update final.go

--- final.go
+++ final.go
@@ -10,14 +10,13 @@ import (
 )
 
 func main() {
-
-			// Reading the mapping.json file and decoding
+				// Reading the mapping.json file and decoding
 	jsonFile, _ := os.Open("mapping.json")
 	defer jsonFile.Close()
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	var result_map map[string]interface{}
 	json.Unmarshal([]byte(byteValue), &result_map)
-			// Dynamically decoding every objects of mapping.json
+				// Dynamically decoding every objects of mapping.json
 	mapping := map[string]map[string]interface{}{}
 	for k,v := range result_map{
 		switch c := v.(type){
@@ -28,13 +27,13 @@ func main() {
 		}
 	}
 
-			//Reading the data.json file and decoding
+				//Reading the data.json file and decoding
 	empJson, _ := os.Open("data.json")
 	defer empJson.Close()
 	byteValue_2, _ := ioutil.ReadAll(empJson)
 	var result map[string]interface{}
 	json.Unmarshal([]byte(byteValue_2), &result)
-			// Dynamically decoding every object of data.json
+				// Dynamically decoding every object of data.json
 	data := map[string]map[string]interface{}{}
 	for k, v := range result {
 		switch c := v.(type) {
@@ -43,23 +42,40 @@ func main() {
 		case []interface{}:
 			for _, item := range v.([]interface{}) {
 				data[k] = item.(map[string]interface{})
+				// fmt.Printf("%v : %v",k, data[k])
+				// fmt.Println("")
 			}
 		default:
 			fmt.Printf("Not sure what type item %q is, but I think it might be %T\n", k, c)
 		}
 	}
 
-			// Taking another map to store keys and values
+				// Taking another map to store keys and values
 	CopiedMap:= make(map[string]interface{})
 	for v := range result_map{
 		for item := range mapping[v] {
 
-			value := (strings.Split(fmt.Sprint((mapping[v][item])),"."))
-			CopiedMap[item] = data[value[0]][value[1]]
-			fmt.Printf("%v : %v\n", item, CopiedMap[item])
-			fmt.Println(" ")
-			
+			tF := strings.Contains(fmt.Sprint(mapping[v][item]), "[")
+			if tF == true {
+				value := (strings.Split(fmt.Sprint((mapping[v][item])),"."))
+				v0 :=  (strings.Split(value[0], "["))
+				for i := range data[v0[0]]{
+					if(value[1] == i){
+						fmt.Printf("%v : %v\n", item, data[v0[0]][i])
+					}
+				}
+				CopiedMap[item] = data[v0[0]][value[1]]
+				//fmt.Printf("%v : %v\n", item, CopiedMap[item])
+			}else{
+				value := (strings.Split(fmt.Sprint((mapping[v][item])),"."))
+				CopiedMap[item] = data[value[0]][value[1]]
+				fmt.Printf("%v : %v\n", item, CopiedMap[item])
+				
+			}
 		}
 	}
+
+	// fmt.Println(data["push_messages[]"]["]status"])
+	
 
 }
